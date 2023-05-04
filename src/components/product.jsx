6bcdfeaff6f629ba15b58dc679facf4505e82372
charlotte divine <divinemaina@gmail.com>
2023-05-04 13:05:40 +0200

    and styles with tailwindcss

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect} from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import { setProducts } from '../redux/productSlice';
-
+import classes from './products.module.css'
 function ProductList() {
   const products = useSelector(state => state.product.products);
   const dispatch = useDispatch();
@@ -17,19 +17,19 @@ function ProductList() {
 
   console.log("Products:", products);
   return (
-    <div>
+    <div className={classes.container}>
     {products.map((product) => (
-      <div key={product.id}>
-        <img src={product.images} alt={product.name} />
-        <h3>{product.name}</h3>
+      <div className={classes.card} key={product.id}>
+        <img className={classes.image} src={product.images} alt={product.name} />
+        <h3 className='text-xl text-blue-500'>{product.name}</h3>
         <p>{product.description}</p>
         <p>Price: {product.price}</p>
         <p>{product.isAvailable ? 'In Stock' : 'Out of Stock'}</p>
         <p>{product.isExpired ? 'Expired' : 'Not Expired'}</p>
         {product.isAvailable && !product.isExpired && (
             <div>
-              <button onClick={() => addToCart(product)}>Add to Cart</button>
-              <button onClick={() => removeFromCart(product)}>Remove</button>
+              <button  className={classes.buttons} onClick={() => addToCart(product)}>Add to Cart</button>
+              <button className={classes.buttons}  onClick={() => removeFromCart(product)}>Remove</button>
       </div> )}</div>
     ))}
   </div>
